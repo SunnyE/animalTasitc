4f53bfc2ca5e729ec8559e9643a587733c3ecd77
Ethan Favia <efavia215@live.com>
2016-06-20 13:37:09 -0400

    logic done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,42 @@
 var animals = [];
 function displaygifs() {
 	$('#gifArea').empty();
-
 	var word = $(this).attr('value');
 	console.log(word);
 	var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + word + '&limit=10&api_key=dc6zaTOxFJmzC'  
 	$.ajax({url: queryURL, method:'GET'})
 		.done(function(response){
 		console.log(response);
+		var gifs = response.data;
 
+		for (var i=0; i<gifs.length; i++){
+			var gifDiv = $('<div>').attr('class', 'searchedDiv');
+			gifDiv.append($('<img>',{
+				class:'gifclick',
+				state: 'still',
+				src: gifs[i].images.original_still.url,
+				datastill: gifs[i].images.original_still.url,
+				dataAnimate: gifs[i].images.original.url,
+			}))
+			//gifDiv.append(img.attr('src', ))
+			gifDiv.append($('<p>').html('Rating: '+ gifs[i].rating))
+			$('#gifArea').append(gifDiv);
+		}
+		return false;
 	});	
-	return false;
+	
 }	
+function startStop () {
+	var state = $(this).attr('state');
+	if(state === 'still') {
+		$(this).attr('state', 'animate');
+		$(this).attr('src', $(this).attr('dataAnimate'));
+	} else if(state === 'animate'){
+		$(this).attr('state', 'still');
+		$(this).attr('src', $(this).attr('datastill'));
+	}
+}
+
 $("#smt").on('click', function() {
 	$('#buttonPrint').empty();
 	animals.push($('#search').val());
@@ -29,3 +54,4 @@ $("#smt").on('click', function() {
 
 $(document).on('click', '.gifsearch', displaygifs);
 
+$(document).on('click', '.gifclick', startStop);
